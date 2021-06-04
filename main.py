12052ed9c8d8d036aed7a32d6922c25c17d693bb
Jeremiah Cummings <jjc15h@my.fsu.edu>
2021-06-03 20:47:51 -0400

    Grabs the name of the first 300 or so games in the "New" Section

--- main.py
+++ main.py
@@ -2,42 +2,74 @@ import requests
 import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
-browser = webdriver.Chrome()
-browser.get("https://store.steampowered.com/search/?sort_by=Released_DESC&os=win")
-browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-#---
-SCROLL_PAUSE_TIME = 0.5
-
-# Get scroll height
-last_height = browser.execute_script("return document.body.scrollHeight")
-a = 0
-while True:
-    # Scroll down to bottom
+def store_category(link):
+    browser = webdriver.Chrome()
+    browser.get(link)
     browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-    # Wait to load page
-    time.sleep(SCROLL_PAUSE_TIME)
+    # ---
+    SCROLL_PAUSE_TIME = 0.5
 
-    # Calculate new scroll height and compare with last scroll height
-    new_height = browser.execute_script("return document.body.scrollHeight")
-    a = a + 1
-    if a == 5:
-        break
+    # Get scroll height
+    last_height = browser.execute_script("return document.body.scrollHeight")
+    a = 0
+    while True:
+        # Scroll down to bottom
+        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-#---
+        # Wait to load page
+        time.sleep(SCROLL_PAUSE_TIME)
 
-soup = BeautifulSoup(browser.page_source, 'html.parser')
-
-new_category = []
-i=0
-for div in soup.find_all("div", {"class": "search_results"}):
-    for link in div.select("a"):
-        new_category.append(link['href'])
-        i = i+1
-        if i == 500:
+        # Calculate new scroll height and compare with last scroll height
+        new_height = browser.execute_script("return document.body.scrollHeight")
+        a = a + 1
+        if a == 6:
             break
 
-print(*new_category, sep='\n')
+    soup = BeautifulSoup(browser.page_source, 'html.parser')
+
+    new_category = []
+    i = 0
+    for div in soup.find_all("div", {"class": "search_results"}):
+        for link in div.select("a"):
+            new_category.append(link['href'])
+            i = i+1
+            if i == 500:
+                break
+
+    return new_category
+
+
+def get_store_data(store):
+    response = requests.get(store)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Gets the name of the game.
+    game_name = soup.find('span', attrs={'itemprop': 'name'}).next
+    print(game_name)
+
+    # To find the price Work on Later
+    # all = soup.find('meta', attrs={'itemprop':'price'})
+    # all2 = all.get('content', '')
+    # print(all2)
+    # https://store.steampowered.com/app/1264280/Slipways/
+    # https://store.steampowered.com/app/601840/Griftlands/
+
+
+# Sorts by New and Trending
+New_List = store_category("https://store.steampowered.com/search/?sort_by=Released_DESC&os=win")
+
+for i in New_List:
+    get_store_data(i)
+
+
+# Sorts by Top Sellers
+# Top_List = store_category("https://store.steampowered.com/search/?filter=topsellers&os=win")
+
+# Sorts by Upcoming
+# Upcoming_List = store_category("https://store.steampowered.com/search/?filter=comingsoon&os=win")
+
+# Sorts by Special
+# Special_List = store_category("https://store.steampowered.com/search/?&specials=1")
